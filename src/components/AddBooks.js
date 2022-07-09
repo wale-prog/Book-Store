3e974ca74f8f46f9f55210104cd8ac51b28d2106
Olawale <olapetanwale@outlook.com>
2022-07-09 23:57:38 +0100

    Add onSubmit to form input

--- src/components/AddBooks.js
+++ src/components/AddBooks.js
@@ -1,11 +1,51 @@
-import React from 'react';
+/* eslint-disable */
+
+import React, { useState } from 'react';
 import './Addbooks.css';
 
-const AddBooks = () => (
-  <form className="form">
-    <input type="text" placeholder="Title" name="title" />
-    <input type="text" placeholder="Author" name="author" />
-    <button type="submit">Submit</button>
-  </form>
-);
+const AddBooks = (props) => {
+  const [input, setInput] = useState({
+    title: '',
+    author: '',
+  });
+ 
+  const handleKeydown = (event) => {
+    setInput({
+      ...input,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if(input.title.trim() && input.author.trim()){
+      props.handleAddbooks(input.title, input.author);
+      setInput({
+        title: ''
+      })
+    }else {
+      alert('Please input title and author')
+    }    
+  }
+  return (
+    <form className="form">
+      <input
+        type="text"
+        placeholder="Title"
+        name="title"
+        value={input.title}
+        onChange={handleKeydown}
+        onSubmit={handleSubmit}
+      />
+      <input
+        type="text"
+        placeholder="Author"
+        name="author"
+        value={input.author}
+        onChange={handleKeydown}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
 export default AddBooks;
