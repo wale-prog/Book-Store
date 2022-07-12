8986167ab6e3ddd68f39b857aef9c7f39f0d361c
Olawale <olapetanwale@outlook.com>
2022-07-12 15:58:52 +0100

    Remove output codes used for testing

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,21 +1,15 @@
 const CHECK_STATUS = 'bookstore/categories/CHECK_STATUS';
-const STATUS_COMPLETE = 'STATUS_COMPLETE';
+
 const initialState = [];
 const categoriesReducer = (state = initialState, action) => {
   switch (action.type) {
     case CHECK_STATUS:
       return [...state, 'Page Under Construction'];
-    case STATUS_COMPLETE:
-      return [...state, 'Page is now available'];
     default: return state;
   }
 };
 export const checkStatus = () => ({
   type: CHECK_STATUS,
-});
-
-export const statusComplete = () => ({
-  type: STATUS_COMPLETE,
 });
 
 export default categoriesReducer;
